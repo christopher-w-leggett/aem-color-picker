e6fa5a2ef4bf84f6e168d0c700d1c7b81102cbee
Christopher Leggett <christopher.leggett@icfi.com>
2018-06-05 10:20:26 -0400

    Changing TextHighlight command to use pipeline.

--- aem-rte-enhancements-ui/src/main/content/jcr_root/apps/aem-rte-enhancements/clientlibs/touch-author/js/rte/commands/TextHighlight.js
+++ aem-rte-enhancements-ui/src/main/content/jcr_root/apps/aem-rte-enhancements/clientlibs/touch-author/js/rte/commands/TextHighlight.js
@@ -27,8 +27,58 @@ RTEExt.rte.commands = RTEExt.rte.commands || {};
         },
 
         execute: function(execDef){
-            var styler = new RTEExt.rte.MarkupStyler('mark');
-            styler.style(execDef.selection, {'background-color': execDef.value}, execDef.editContext.root);
+            var startNode = execDef.selection.startNode,
+                endNode = execDef.selection.endNode,
+                root = execDef.editContext.root,
+                actingRoot = RTEExt.rte.Utils.getCommonAncestor(startNode, endNode, root, function(node){
+                    return node.tagName
+                        && (RTEExt.rte.Utils.isContainerNode(node) || RTEExt.rte.Utils.isStylingContainerNode(node));
+                }),
+                generator = new RTEExt.rte.selection.pipeline.HtmlSelectionGenerator(
+                    startNode,
+                    execDef.selection.startOffset,
+                    endNode,
+                    execDef.selection.endOffset,
+                    actingRoot
+                ),
+                serializer = new RTEExt.rte.selection.pipeline.HtmlSelectionSerializer(
+                    actingRoot,
+                    function(documentFragment){
+                        RTEExt.rte.Utils.normalize(
+                            documentFragment,
+                            function(node){
+                                return !RTEExt.rte.Utils.isContainerNode(node)
+                                    && !RTEExt.rte.Utils.isStylingContainerNode(node)
+                                    && !RTEExt.rte.Utils.isIgnoredNode(node);
+                            },
+                            function(node){
+                                var strip = false;
+
+                                //TODO: Verify <br/>, <img/> and other self closing tags won't have an issue here.
+                                if(node.nodeType === 1
+                                    && !RTEExt.rte.Utils.isContainerNode(node)
+                                    && !RTEExt.rte.Utils.isStylingContainerNode(node)
+                                    && !RTEExt.rte.Utils.isIgnoredNode(node)
+                                    && !node.firstChild){
+                                    strip = true;
+                                } else if(node.nodeType === 3 && node.textContent.length === 0) {
+                                    strip = true;
+                                }
+
+                                return strip;
+                            }
+                        );
+                    }
+                ),
+                pipeline = new RTEExt.rte.selection.pipeline.Pipeline(generator, serializer);
+
+            //add transformer
+            pipeline.addTransformer(new RTEExt.rte.selection.pipeline.StylingHandler(
+                'mark', {'background-color': execDef.value}
+            ));
+
+            //style
+            pipeline.run();
         }
     });
 
